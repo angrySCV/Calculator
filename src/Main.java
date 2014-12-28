24d3b2bf5b5bfb3673007817364094178acc84fc
Dima Kubitskiy <dima@bepromo.ru>
2014-12-28 04:20:54 +0300

    поиск полного совпадения строки

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,19 @@
-import com.tsystems.javaschool.tasks.Calculator;
-import com.tsystems.javaschool.tasks.CalculatorImpl;
+import com.tsystems.javaschool.tasks.Subsequence;
+
+import java.util.Arrays;
 
 public class Main {
 
 	public static void main (String[] args) {
 
-		Calculator calculator = new CalculatorImpl();
+//		Calculator calculator = new CalculatorImpl();
 
-		System.out.println(calculator.evaluate("(1+38)*4-5"));
-// простая проверка примера - ожидаемый результат 151
+//		System.out.println(calculator.evaluate("(1+38)*4-5"));
+        // простая проверка примера - ожидаемый результат 151
+
+		Subsequence s = new SubsequenceImpl();
+		boolean b = s.find(Arrays.asList("A", "B", "C", "D"), Arrays.asList("BD", "A", "ABC", "B", "M", "D", "M", "C", "DC", "D"));
+		System.out.println(b); // ожидаемый результат: true
 	}
 
 }
---
+++ src/SubsequenceImpl.java
@@ -0,0 +1,32 @@
+import com.tsystems.javaschool.tasks.Subsequence;
+
+import java.util.List;
+
+/**
+ * Created by AngrySCV on 28.12.14.
+ * https://github.com/angrySCV
+ */
+public class SubsequenceImpl implements Subsequence {
+	private int kolichestvoSovpadeniy = 0;
+	private int startElement = 0;
+
+	@Override
+	public boolean find (List x, List y) {
+		int i = 0;
+		while (i < x.size()) { // здесь простой перебор совпадений в строках
+			kolichestvoSovpadeniy = 0;
+//			startElement = i;
+			for (int j = 0; j < y.size(); j++) {
+
+				if (x.get(i).equals(y.get(j))) {
+					i++;
+					kolichestvoSovpadeniy++;
+				}
+			}
+		if (kolichestvoSovpadeniy == x.size()) return true;
+			else return false; // если при первом же элементе нет совпадения то дальше не проверяем - совпадения строки и не будет
+		}
+		return false;
+	}
+
+}
